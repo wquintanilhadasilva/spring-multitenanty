104b743619629cb6b2baa36caef44dc798817458
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-09-06 14:14:21 -0300

    add lombok

--- poc2/src/main/java/br/basis/lab/multitenanty/model/City.java
+++ poc2/src/main/java/br/basis/lab/multitenanty/model/City.java
@@ -3,37 +3,24 @@ package br.basis.lab.multitenanty.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "city")
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class City implements Serializable {
     private static final long serialVersionUID = -4551953276601557391L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String toString() {
